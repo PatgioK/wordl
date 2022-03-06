47d838a623934c02377dea621439417f77dc3ec2
Patgio <Patgiok@gmail.com>
2022-03-06 13:55:38 -0800

    add axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import './App.css';
-import Wordl from './components/wordl.js';
-import Home from "./components/Home.js";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 
 const apiKey = process.env.REACT_APP_NASA_KEY;
@@ -9,31 +6,23 @@ const apiKey = process.env.REACT_APP_NASA_KEY;
 function App() {
   const [bg, setBg] = useState(null);
 
+  async function fetchPhoto() {
+    const dat = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
+    const data = await dat.json();
+    setBg(data); 
+  }
+
   useEffect(() => {
-    fetchPhoto();
-    async function fetchPhoto() {
-      const dat = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
-      const data = await dat.json();
-      setBg(data);
-    }
+    fetchPhoto()
   }, [])
+  
+  useEffect(() => {
+    if(bg){
+      document.body.style.backgroundImage = `url('${bg.url}')`;
+    }
+  }, [bg])
 
-  console.log(apiKey)
-  // console.log(bg)
-  if (!bg) return <div />;
-
-  return (
-    // <div className="App">
-    //   <Home />
-    // </div>
-    <div className="nasa-photo">
-      <img
-        src={bg.url}
-        alt={bg.title}
-        className="photo"
-      />
-    </div>
-  );
+  return null;
 }
 
 export default App;
